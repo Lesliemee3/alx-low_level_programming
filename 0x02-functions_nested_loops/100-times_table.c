80704fe7f04a1ded90e81cd3bcb08756f452e3cb
OSAMUYIMEN LESLIE OGIESOBA-EGUAKUN <lesliemee3@gmail.com>
2023-02-21 07:43:38 -0800

    update

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * print_times_table - prints the times table, starting with 0
+ * @n: number of the times table
+ */
+void print_times_table(int n)
+
+{
+int i = 0, rep, j;
+
+if (n < 0 || n > 15)
+	return;
+
+while (i <= n)
+
+{
+	for (j = 0; j <= n; j++)
+
+	{
+		rep = i * j;
+		if (j == 0)
+
+			_putchar(rep + '0');
+		else if (rep < 10)
+
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(rep + '0');
+		}
+		else if (rep < 100)
+
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+	i++;
+}
+}
