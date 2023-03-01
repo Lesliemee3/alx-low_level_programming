eafd7d4a7f76e20810532c2c53be38178e23f213
OSAMUYIMEN LESLIE OGIESOBA-EGUAKUN <lesliemee3@gmail.com>
2023-03-01 12:27:30 -0800

    update

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main(void)
 
+int main(void)
 {
 int n;
 int a[5];
@@ -21,4 +21,5 @@ p = &n;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
+
 }
